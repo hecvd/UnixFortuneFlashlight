6af546e6f3933747bfad7744aa4cffc71d41ae5e
Hector V. Diaz <hc.vdiaz@gmail.com>
2014-09-03 17:23:50 -0500

    VersionCode and VersionName specified. Min API level set to 15

--- app/src/main/java/com/hecvd/fortuneflashlight/camera/FlashlightHandler.java
+++ app/src/main/java/com/hecvd/fortuneflashlight/camera/FlashlightHandler.java
@@ -1,6 +1,7 @@
 package com.hecvd.fortuneflashlight.camera;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.util.Log;
@@ -17,6 +18,9 @@ public class FlashlightHandler {
 
 	public FlashlightHandler(Context context) {
 		this.context = context;
+		if(!this.context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+			Toast.makeText(this.context,"Torch flash not supported",Toast.LENGTH_LONG).show();
+		}
 	}
 
 	private void getCamera() {
@@ -39,12 +43,12 @@ public class FlashlightHandler {
 				Toast.makeText(this.context,"Camera.release() failed: " + e.getMessage(),Toast.LENGTH_LONG).show();
 			}
 		}
+		camera = null;
 	}
 
 	public void toggleLight() {
 		getCamera();
 		privateToggleLight();
-		releaseCamera();
 	}
 
 	private void privateToggleLight() {
@@ -58,13 +62,14 @@ public class FlashlightHandler {
 			return;
 		}
 		String flashMode = parameters.getFlashMode();
-		Log.i(TAG, "Flash mode: " + flashMode);
-		Log.i(TAG, "Flash modes: " + flashModes);
+//		Log.i(TAG, "Flash mode: " + flashMode);
+//		Log.i(TAG, "Flash modes: " + flashModes);
 		//Toast.makeText(this.context,"Flash modes: " + flashModes,Toast.LENGTH_LONG).show();
 		if (Parameters.FLASH_MODE_OFF.equals(flashMode)){
 			if (flashModes.contains(Parameters.FLASH_MODE_TORCH)) {
 				parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
 				camera.setParameters(parameters);
+				camera.startPreview();
 			}else{
 				Toast.makeText(this.context,"FLASH_MODE_TORCH not supported",Toast.LENGTH_LONG).show();
 				Log.e(TAG, "FLASH_MODE_TORCH not supported");
@@ -73,11 +78,12 @@ public class FlashlightHandler {
 			if (flashModes.contains(Parameters.FLASH_MODE_OFF)) {
 				parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
 				camera.setParameters(parameters);
+				camera.stopPreview();
+				releaseCamera();
 			}else{
 				Log.e(TAG, "FLASH_MODE_OFF not supported");
 				Toast.makeText(this.context,"FLASH_MODE_OFF not supported",Toast.LENGTH_LONG).show();
 			}
 		}
 	}
-
 }
